65ad5485eb0ddedf45dc8f48e8d14d1d10cd943d
Johnson Joseph <jhonsonj88@gmail.com>
2019-02-28 05:38:17 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,3 +1,10 @@
 class Transfer
-  # your code here
+
+  attr_accessor :transfer, :sender, :receiver
+
+  def initialize(transfer, sender, receiver)
+    @transfer = Transfer
+    @sender = sender
+  end
+
 end
