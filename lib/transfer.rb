f9f7906420788a9f3704c1953575255cc7a5f24a
Johnson Joseph <jhonsonj88@gmail.com>
2019-02-28 06:20:41 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,7 +16,7 @@ class Transfer
   end
 
   def execute_transaction
-      binding.pry
+      #binding.pry
     if @sender.balance > @amount && @status == "pending"
       @sender.balance -= @amount
       @receiver.balance += @amount
