25bea72b22fd283aea5c4363c69b78fde926badd
Johnson Joseph <jhonsonj88@gmail.com>
2019-02-28 06:18:26 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,10 +16,9 @@ class Transfer
   end
 
   def execute_transaction
-    if @sender.balance > @amount && @status == "pending"
+    if @sender.balance > @amount && @status == "complete"
       @sender.balance -= @amount
       @receiver.balance += @amount
-      @status = "complete"
     else
       @status = "rejected"
       return "Transaction rejected. Please check your account balance."
