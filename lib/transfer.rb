a8109e77ee033cbdf3e6b0308985a829d73d0436
Johnson Joseph <jhonsonj88@gmail.com>
2019-02-28 06:01:58 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -16,7 +16,7 @@ class Transfer
   end
 
   def execute_transaction
-    status == "complete"
+    self.status == "complete"
     sender.balance -= amount
     receiver.balance += amount
     self.status = "complete "
