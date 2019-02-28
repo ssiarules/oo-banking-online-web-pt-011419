3cdf3f369a6345a2bdea47d5efe1787b9dae806e
Johnson Joseph <jhonsonj88@gmail.com>
2019-02-28 05:45:24 +0000

    Automatically backed up by Learn

--- lib/transfer.rb
+++ lib/transfer.rb
@@ -1,11 +1,12 @@
 class Transfer
 
-  attr_accessor :money, :sender, :receiver, :status
+  attr_accessor :money, :sender, :receiver, :status, :amount 
 
   def initialize(sender, receiver, money)
     @sender = sender
     @receiver = receiver
     @status = "pending"
+    
   end
 
 end
